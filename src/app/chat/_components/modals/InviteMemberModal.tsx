6f93591a6d3748df8b1a93c2605b22624309a0b4
Deearss <haidiraditya16@gmail.com>
2026-05-26 07:25:12 +0800

    UI: Perbaiki z-index InviteMemberModal agar tidak tertutup sidebar GroupInfoModal

--- src/app/chat/_components/modals/InviteMemberModal.tsx
+++ src/app/chat/_components/modals/InviteMemberModal.tsx
@@ -73,7 +73,7 @@ export default function InviteMemberModal({
   return (
     <AnimatePresence>
       {isOpen && (
-        <div className="fixed inset-0 z-50 flexcc p-4">
+        <div className="fixed inset-0 z-60 flexcc p-4">
           <motion.div 
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
